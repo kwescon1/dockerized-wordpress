0356c04a0025e4abcaa14656483e9ea40faf9b55
Odame <kwesi.danquah@daaseng.com>
2023-10-12 22:23:22 +0100

    Refactor entrypoint script: Add dynamic DB configuration using environment variables

--- docker-files/php/entrypoint.sh
+++ docker-files/php/entrypoint.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 
-if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
-    echo "WordPress not found in /var/www/wordpress - copying now..."
-    if [ "$(ls -A /var/www/wordpress)" ]; then
-        echo "WARNING: /var/www/wordpress is not empty - aborting copy"
+WP_PATH="/var/www/wordpress"
+CONFIG_FILE="$WP_PATH/wp-config.php"
+SAMPLE_CONFIG_FILE="$WP_PATH/wp-config-sample.php"
+
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "WordPress not found in $WP_PATH - copying now..."
+    if [ "$(ls -A $WP_PATH)" ]; then
+        echo "WARNING: $WP_PATH is not empty - aborting copy"
     else
-        cp -a /var/www/wordpress_backup/. /var/www/wordpress/
-        echo "WordPress copied to /var/www/wordpress"
+        cp -a /var/www/wordpress_backup/. $WP_PATH/
+        echo "WordPress copied to $WP_PATH"
+
+        # Copy wp-config-sample.php to wp-config.php
+        cp $SAMPLE_CONFIG_FILE $CONFIG_FILE
+
+        # Edit wp-config.php using sed
+        sed -i "s/'database_name_here'/getenv('WORDPRESS_DB_NAME')/g" $CONFIG_FILE
+        sed -i "s/'username_here'/getenv('WORDPRESS_DB_USER')/g" $CONFIG_FILE
+        sed -i "s/'password_here'/getenv('WORDPRESS_DB_PASSWORD')/g" $CONFIG_FILE
+        sed -i "s/'localhost'/getenv('WORDPRESS_DB_HOST')/g" $CONFIG_FILE
+
+        echo "$CONFIG_FILE has been updated"
     fi
 else
-    echo "Using existing WordPress in /var/www/wordpress"
+    echo "Using existing WordPress in $WP_PATH"
 fi
 
 # Continue with normal fpm start
